f100727fb4dbe81959de20626777ea50645a55f9
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2023-11-19 10:07:22 +0100

    Update TKM_2018_19_K01_Z01_A_Nadovezivanje_Bod_020_100.cs

--- TKM_2018_19/TKM_2018_19_K01/TKM_2018_19_K01_Z01_A_Nadovezivanje/TKM_2018_19_K01_Z01_A_Nadovezivanje_Bod_020_100.cs
+++ TKM_2018_19/TKM_2018_19_K01/TKM_2018_19_K01_Z01_A_Nadovezivanje/TKM_2018_19_K01_Z01_A_Nadovezivanje_Bod_020_100.cs
@@ -1,7 +1,7 @@
 // https://arena.petlja.org/competition/20182019kvalifikaciie2-vezba#tab_129377
 
 using System;
-class Tkm_2018_19_K01_Z01_A_Nadovezivanje_Bod_020
+class Tkm_2018_19_K01_Z01_A_Nadovezivanje_Bod_100
 {
     static void Razmeni(ref string a, ref string b)
     {
@@ -9,13 +9,18 @@ class Tkm_2018_19_K01_Z01_A_Nadovezivanje_Bod_020
         a = b;
         b = p;
     }
+    static void Sortiraj(ref string x)
+    {
+        string[] s = x.Split();
+        if ((s[0] + s[1]).CompareTo(s[1] + s[0]) < 0) Razmeni(ref s[0], ref s[1]);
+        if ((s[0] + s[2]).CompareTo(s[2] + s[0]) < 0) Razmeni(ref s[0], ref s[2]);
+        if ((s[1] + s[2]).CompareTo(s[2] + s[1]) < 0) Razmeni(ref s[1], ref s[2]);
+        x = s[0] + s[1] + s[2];
+    }
     static void Main()
     {
-        string[] s = Console.ReadLine().Split();
-        if (s[0].CompareTo(s[1]) < 0) Razmeni(ref s[0], ref s[1]);
-        if (s[0].CompareTo(s[2]) < 0) Razmeni(ref s[0], ref s[2]);
-        if (s[1].CompareTo(s[2]) < 0) Razmeni(ref s[1], ref s[2]);
-        string r = s[0] + s[1] + s[2];
-        Console.WriteLine(r);
+        string s = Console.ReadLine();
+        Sortiraj(ref s);
+        Console.WriteLine(s);
     }
 }
